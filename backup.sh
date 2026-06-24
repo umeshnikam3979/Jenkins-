304dd3e64efe1b5cb780fabc06dda011faa1b2cf
umeshnikam3979 <130720684+umeshnikam3979@users.noreply.github.com>
2026-06-24 07:42:12 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,11 +4,9 @@ DB_USER="manish"
 DB_PASS="manish@123"
 DB_NAME="employee_db"
 
-BACKUP_DIR="/home/umesh/SQL"
+BACKUP_DIR="/var/lib/jenkins/workspace/database"
 DATE=$(date +%F_%H-%M-%S)
 
-mkdir -p $BACKUP_DIR
+mysqldump -u$DB_USER -p$DB_PASS --no-tablespaces $DB_NAME > ${BACKUP_DIR}/employee_db_${DATE}.sql
 
-mysqldump -u$DB_USER -p$DB_PASS --no-tablespaces $DB_NAME > $BACKUP_DIR/${DB_NAME}_${DATE}.sql
-
-echo "Backup completed: $BACKUP_DIR/${DB_NAME}_${DATE}.sql"
+echo "Backup completed: ${BACKUP_DIR}/employee_db_${DATE}.sql"
